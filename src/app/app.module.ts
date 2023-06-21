1d200b3ca649ab7314cc33786d3d2e4a1e7a1f10
zakaria benmessaoud <aitbenmesszakaria@gmail.com>
2023-06-21 19:39:15 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { KeyWordSectionComponent } from './key-word-section/key-word-section.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { TweetsSectionComponent } from './tweets-section/tweets-section.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { ChartsModule } from '@rinminase/ng-charts';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    KeyWordSectionComponent,
+    TweetsSectionComponent,
+    PieChartComponent,
+    BarChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
